db8945bde59f5cba23ce9c99d013a35c32a2670c
Chow Lye Fong <34432443+chowlyefong@users.noreply.github.com>
2019-02-15 01:14:06 +0800

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 $(document).on('pageinit', '#loginPage', function(){  
         $(document).on('click', '#submit', function() { // catch the form's submit event
-            if($('#usrname').val().length > 0 && $('#pswrd').val().length > 0){
+            if ($('#usrname').val().length > 0 && $('#pswrd').val().length > 0){
 				if ($('#pswrd').val().length < 6 || $('#pswrd').val().length > 20){
-					$('#errorMsg').style.display = 'block';
-					$('#errMsg').innerText = 'Password must be between 6 to 20 characters length';
+					window.document.getElementById('errMsg').innerText = 'Password must be between 6 to 20 characters length';
+					window.document.getElementById('errorMsg').style.display = 'block';
 				}else{
 					// Send data to server through the Ajax call
 					// action is functionality we want to call and outputJSON is our data
@@ -21,25 +21,27 @@ $(document).on('pageinit', '#loginPage', function(){
                             $.mobile.hidePageLoadingMsg(); // This will hide ajax spinner
                         },
                         success: function (result) {
+							alert(result.status);
                             if(result.status) {
                                 $.mobile.changePage("#message");                         
                             } else {
-                                $('#errorMsg').style.display = 'block';
-								$('#errMsg').innerText = 'Login Successful';
+                                window.document.getElementById('errMsg').innerText = 'Login Not Successful';
+								window.document.getElementById('errorMsg').style.display = 'block';
                             }
                         },
                         error: function (request,error) {
                             // This callback function will trigger on unsuccessful action 
-							$('#errorMsg').style.display = 'block';
-							$('#errMsg').innerText = 'Network error has occurred please try again';
+							window.document.getElementById('errMsg').innerText = 'Network error has occurred please try again';
+							window.document.getElementById('errorMsg').style.display = 'block';
+							
                         }
                     });
 					$('#usrname').value = '';
 					$('#pswrd').value = '';
 				}
             } else {
-                $('#errorMsg').style.display = 'block';
-				$('#errMsg').innerText = 'Please enter Username and Password to login';
+				window.document.getElementById('errMsg').innerText = "Please enter Username and Password to login";
+				window.document.getElementById('errorMsg').style.display = "block";
             }           
             return false; // cancel original event to prevent form submitting
         });    
